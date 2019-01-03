831c7b6a53e8f74ef23b959f3bef81b6fdf2217c
Edycarlos018 <edycarlos@gmail.com>
2019-01-03 13:13:50 -0500

    added the register

--- gardenprojectangular/src/app/app.module.ts
+++ gardenprojectangular/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { FlowerrequestComponent } from './flowerrequest/flowerrequest.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service';
+import { RegistrationComponent } from './registration/registration.component';
+import {
+  MatToolbarModule,
+  MatInputModule,
+  MatButtonModule,
+  MatFormFieldModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -14,13 +25,24 @@ import { FlowerrequestComponent } from './flowerrequest/flowerrequest.component'
     AboutComponent,
     ContactComponent,
     FlowerrequestComponent,
-    
+    LoginComponent,
+    RegistrationComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
